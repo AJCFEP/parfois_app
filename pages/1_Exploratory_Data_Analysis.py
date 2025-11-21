835c0dd043d9a7e0979a45db5ec4eab5526ec2c7
AJCFEP <up199401247@edu.fep.up.pt>
2025-11-21 16:17:35 +0000

    header

--- pages/1_Exploratory_Data_Analysis.py
+++ pages/1_Exploratory_Data_Analysis.py
@@ -10,7 +10,7 @@ def show_header():
         """
         <style>
             .block-container {
-                padding-top: 1rem;
+                padding-top: 1.5rem;
             }
         </style>
         """,
